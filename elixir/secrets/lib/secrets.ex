4ea9bad47d0283e6891d9dff6f20cf6043cb9836
Jason Lamb <blksheep@tutanota.com>
2022-02-05 12:48:54 +0200

    Fixing failing tests

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,5 +1,5 @@
 defmodule Secrets do
-  import Bitwise
+  use Bitwise
 
   def secret_add(secret), do: &(&1 + secret)
 
@@ -7,12 +7,12 @@ defmodule Secrets do
 
   def secret_multiply(secret), do: &(&1 * secret)
 
-  def secret_divide(secret), do: &(&1 / secret)
+  def secret_divide(secret), do: &div(&1, secret)
 
   def secret_and(secret), do: &(&1 &&& secret)
 
-  def secret_xor(secret), do: &(&1 ||| secret)
+  def secret_xor(secret), do: &bxor(&1, secret)
 
   def secret_combine(secret_function1, secret_function2),
-    do: secret_function1.() |> secret_function2.()
+    do: fn x -> x |> secret_function1.() |> secret_function2.() end
 end
